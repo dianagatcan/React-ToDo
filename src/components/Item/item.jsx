0e666f31906cf3f96cc126cb08bd41b4d63a433a
Diana G <67389035+dianagatcan@users.noreply.github.com>
2022-10-11 17:22:47 +0300

    reafactor item component

--- src/components/Item/item.jsx
+++ src/components/Item/item.jsx
@@ -1,25 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import './styles.css';
 
 export default function Item(props){
-    const [done, setDone] = useState(false);
 
-    function deleteItem(){
+    function handleDelete(){
         props.deleteItem(props.id)
-    }
-   
-    function toggleDone(event){
-        if(done){
-            event.target.style.textDecoration='none'
-        setDone(false)} else{
-            event.target.style.textDecoration='line-through'
-            setDone(true)
-        }
     }
 
     return(
         <ul className="list">
-            <li className="item" title={props.text} onClick={toggleDone}>{props.text}<span className="delete" onClick={deleteItem}>x</span></li>
+            <li className="item" title={props.text}>{props.text}<span className="delete" onClick={handleDelete}>x</span></li>
         </ul>
         
     )
